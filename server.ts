6429d13c376e1f5413a4305dcdd6b83e1f3de340
jz <zjcxjz@163.com>
2021-07-18 18:36:25 +0800

    produce ok

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
+
 const mediasoup = require('mediasoup');
 const express = require('express');
 const config = require('./config/config.js')
+const protoo = require('protoo-server');
 
 let app = express();
 
@@ -10,20 +12,28 @@ let producerTransport;
 let consumerTransport;
 let producer;
 let consumer;
+// let protooServer;
+// let peer;
+// let protooRoom;
+// let protooTransport;
 
 mediasoup.createWorker(
     config.mediasoup.workerSettings
 ).then(
     (worker)=>{
         myWorker = worker;
-        const codecs = config.mediasoup.mediaCodecs
+        const {mediaCodecs} = config.mediasoup.routerOptions
         worker.createRouter(
-            {codecs}
+            {mediaCodecs}
         ).then((router)=>{
             myRouter = router
         })
     }
 )
+
+
+
+app.use(express.json());
 
 app.get(
     '/getRouterRtpCapabilities',
@@ -35,7 +45,7 @@ app.get(
 
 app.post(
     '/createProducerTransport',
-    async function (req, res){
+    async (req, res, next)=>{
         const {sctpCapabilities} = req.body
         producerTransport = await myRouter.createWebRtcTransport({
             ...config.mediasoup.webRtcTransportOptions,
@@ -79,8 +89,10 @@ app.post(
     '/connectTransport',
     async function (req, res){
         const { dtlsParameters } = req.body;
+        console.log('[Connect]')
+        console.log(req.body)
         await producerTransport.connect({dtlsParameters});
-        await res.status(200).json();
+        await res.status(200).json({});
     }
 )
 
@@ -88,7 +100,10 @@ app.post(
     '/createProducer',
     async function (req, res){
         const { kind, rtpParameters } = req.body;
+        console.log('[createProducer]')
+        console.log(req.body)
         producer = await producerTransport.produce({kind, rtpParameters});
+        await res.status(200).json({id: producer.id});
     }
 )
 
@@ -110,9 +125,71 @@ app.post(
     }
 )
 
+app.post(
+    '/produce',
+    async (req, res)=>{
+        let {kind,rtpParameters, appData} = req.body;
+        console.log('[Produce]');
+        console.log(req.body);
+        const producer = await producerTransport.produce(
+            {
+                kind,
+                rtpParameters,
+                appData
+            }
+        )
+        const id = producer.id
+        await res.status(200).json({id})
+    }
+)
+
+
+// protooServer = new protoo.WebSocketServer(app,
+//     {
+//         maxReceivedFrameSize     : 960000, // 960 KBytes.
+//         maxReceivedMessageSize   : 960000,
+//         fragmentOutgoingMessages : true,
+//         fragmentationThreshold   : 960000
+//     });
+//
+// protooServer.on('connectionrequest', (info, accept, reject) => {
+//     protooTransport =  accept();
+//     handleProtooConnection();
+// })
+//
+// function handleProtooConnection() {
+//     protooRoom = new protoo.Room();
+//
+//     peer = protooRoom.createPeer(1,protooTransport);
+//
+//     peer.on('request', (request, accept, reject) => {
+//         handleProtooRequest(peer, request, accept, reject);
+//     })
+// }
+
 var server = app.listen(4443, function (){
     var host = server.address().address
     var port = server.address().port
 
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
 })
+
+// async function handleProtooRequest (peer, request, accept, reject)
+// {
+//     switch (request.method)
+//     {
+//         case 'produce':
+//         {
+//             const {kind, rtpParameters} = request.data;
+//             let {appData} = request.data;
+//             console.log(appData);
+//             producer = await producerTransport.produce(
+//                 {
+//                     kind,
+//                     rtpParameters,
+//                     appData
+//                 }
+//             );
+//         }
+//     }
+// }
